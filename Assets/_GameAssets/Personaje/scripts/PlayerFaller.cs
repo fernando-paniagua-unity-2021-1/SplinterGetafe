2938048419303f4ebc7d82e63c4f63b32f61745d
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-03-11 21:16:51 +0100

    Initial commit

---
+++ Assets/_GameAssets/Personaje/scripts/PlayerFaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFaller : MonoBehaviour
+{
+    public Animator animator;
+    private const string PARAM_CAIDA_PLANO = "Neymar";
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("ObstaculoSuelo")){
+            animator.SetTrigger(PARAM_CAIDA_PLANO);
+        }
+    }
+}
---
+++ Assets/_GameAssets/Personaje/scripts/PlayerMover.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public Animator animator;
+
+    public float rotationSpeed;
+    public float walkSpeed;
+    public float runSpeed;
+    public float stealthSpeed;
+    private float x;
+    private float z;  
+
+    private const string PARAM_ANDAR = "Andar";
+    private const string PARAM_CORRER = "Correr";
+    private const string PARAM_STEALTH = "AndarSigilo";
+
+    private void Update() {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        DesactivarAvance();
+        Andar();
+        IntentarCorrer();
+    }
+
+    private void Andar(){
+        if (z>0) {
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetButton("Fire3")){
+                animator.SetBool(PARAM_STEALTH, true);
+                transform.Translate(Vector3.forward * Time.deltaTime * stealthSpeed * z);
+                transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+            } else {
+                animator.SetBool(PARAM_STEALTH, false);
+                animator.SetBool(PARAM_ANDAR, true);
+                transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed * z);
+                transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+            }
+        } else {
+            animator.SetBool(PARAM_ANDAR, false);
+            animator.SetBool(PARAM_STEALTH, false);
+        }
+    }
+    
+    private void IntentarCorrer(){
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetButton("Fire2")){
+            Correr();
+        } 
+        else {
+            animator.SetBool(PARAM_CORRER, false);
+        }
+    }
+    private void Correr(){
+        transform.Translate(Vector3.forward * Time.deltaTime * runSpeed * z);
+        if (z>0) {
+            animator.SetBool(PARAM_CORRER, true);
+            transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        } else {
+            animator.SetBool(PARAM_CORRER, false);
+        }
+    }
+    private void DesactivarAvance(){
+        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name=="Fall Flat" 
+            ||
+            animator.GetCurrentAnimatorClipInfo(0)[0].clip.name=="Getting Up"){
+            z=0;
+        }
+    }
+}
